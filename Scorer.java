9d4ec66ac4522f4b1fc063502d872669bdb5db13
alexanderswann <32579236+alexanderswann@users.noreply.github.com>
2022-02-21 11:49:23 -0500

    Create Scorer.java

---
+++ Scorer.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class FileSearcher {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+    String file = "C:\Users\Dell\Documents\GitHub\TrackScorer\TuftsCupidChallenge 2-5-22\MensResults.txt";
+    File f = new File(file);
+
+  }
+
+}
